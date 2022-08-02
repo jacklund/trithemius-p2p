f77bfdb9bb430d4813c5b15a57624bf2658251dd
Jack Lund <jackl@geekheads.net>
2022-08-01 23:18:20 -0500

    Got it building with tor-stream, next is tests

--- src/tor/transport.rs
+++ src/tor/transport.rs
@@ -1,15 +1,17 @@
-use async_socks5::{connect, AddrKind, Auth};
-use futures::future::{Future, FutureExt, MapErr, TryFutureExt};
+use futures::future::{FutureExt, MapErr, TryFutureExt};
 use libp2p_core::transport::{ListenerId, Transport, TransportError, TransportEvent};
+use libp2p_tcp::tokio::TcpStream;
 use multiaddr::{Multiaddr, Protocol};
+use parking_lot::Mutex;
 use std::fmt;
 use std::net::SocketAddr;
+use std::ops::DerefMut;
 use std::pin::Pin;
-use tokio::io::{AsyncRead, AsyncWrite};
+use std::sync::Arc;
+use tor_stream::TorStream;
 
 #[derive(Debug)]
 pub enum TorTransportError<TErr> {
-    SOCKS5Error(async_socks5::Error),
     Transport(TErr),
     MultiaddrNotSupported(Multiaddr),
 }
@@ -20,7 +22,6 @@ where
 {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            TorTransportError::SOCKS5Error(err) => write!(f, "{}", err),
             TorTransportError::Transport(err) => write!(f, "{}", err),
             TorTransportError::MultiaddrNotSupported(a) => {
                 write!(f, "Unsupported resolved address: {}", a)
@@ -31,51 +32,46 @@ where
 
 impl<TErr> std::error::Error for TorTransportError<TErr> where TErr: fmt::Debug + fmt::Display {}
 
+impl From<std::io::Error> for TorTransportError<std::io::Error> {
+    fn from(error: std::io::Error) -> Self {
+        TorTransportError::Transport(error)
+    }
+}
+
 pub struct TorTransportWrapper<T> {
-    inner: T,
-    proxy_addr: Multiaddr,
-    auth: Option<Auth>,
+    inner: Arc<Mutex<T>>,
+    proxy_addr: SocketAddr,
 }
 
 impl<T> TorTransportWrapper<T> {
     pub fn new(
         inner: T,
-        proxy_addr: Multiaddr,
-        auth: Option<Auth>,
+        proxy_addr: SocketAddr,
     ) -> Result<TorTransportWrapper<T>, Box<dyn std::error::Error>> {
         Ok(Self {
-            inner,
+            inner: Arc::new(Mutex::new(inner)),
             proxy_addr,
-            auth,
         })
     }
 }
 
-fn convert_multiaddr<T>(mut addr: Multiaddr) -> Result<AddrKind, TorTransportError<T>> {
-    match addr.pop() {
-        Some(Protocol::Tcp(port)) => match addr.pop() {
-            Some(Protocol::Ip4(ipv4)) => Ok(AddrKind::Ip(SocketAddr::new(ipv4.into(), port))),
-            Some(Protocol::Ip6(ipv6)) => Ok(AddrKind::Ip(SocketAddr::new(ipv6.into(), port))),
-            Some(Protocol::Dns(host)) => Ok(AddrKind::Domain(host.to_string(), port)),
-            _ => Err(TorTransportError::MultiaddrNotSupported(addr)),
-        },
-        Some(Protocol::Onion3(address)) => {
-            let onion_address = format!(
-                "{}.onion",
-                base32::encode(base32::Alphabet::RFC4648 { padding: false }, address.hash())
-            );
-            Ok(AddrKind::Domain(onion_address, address.port()))
-        }
-        _ => Err(TorTransportError::MultiaddrNotSupported(addr)),
+fn to_onion3_domain<'a>(addr: Multiaddr) -> Option<(String, u16)> {
+    let mut addr = addr.clone();
+    if let Some(Protocol::Onion3(address)) = addr.pop() {
+        let onion_address = format!(
+            "{}.onion",
+            base32::encode(base32::Alphabet::RFC4648 { padding: false }, address.hash())
+        );
+        Some((onion_address, address.port()))
+    } else {
+        None
     }
 }
 
 impl<T> Transport for TorTransportWrapper<T>
 where
-    T: Transport + Send + Clone + Unpin + 'static,
-    T::Output: AsyncRead + AsyncWrite + Send + Unpin,
+    T: Transport<Output = TcpStream, Error = std::io::Error> + Send + Unpin + 'static,
     T::Dial: Send,
-    T::Error: Send,
 {
     type Output = T::Output;
     type Error = TorTransportError<T::Error>;
@@ -85,57 +81,30 @@ where
 
     fn listen_on(&mut self, addr: Multiaddr) -> Result<ListenerId, TransportError<Self::Error>> {
         self.inner
+            .lock()
             .listen_on(addr)
             .map_err(|e| e.map(TorTransportError::Transport))
     }
 
     fn remove_listener(&mut self, id: ListenerId) -> bool {
-        self.inner.remove_listener(id)
+        self.inner.lock().remove_listener(id)
     }
 
     fn dial(&mut self, addr: Multiaddr) -> Result<Self::Dial, TransportError<Self::Error>> {
         let dest_addr = addr.clone();
         let proxy_addr = self.proxy_addr.clone();
-        let auth = self.auth.clone();
-        let mut inner = self.inner.clone();
+        let inner = self.inner.clone();
         Ok(async move {
-            let addr_kind = convert_multiaddr(dest_addr.clone())?;
-            if let AddrKind::Domain(host, _port) = addr_kind.clone() {
-                if host.ends_with(".onion") {
-                    match inner.dial(proxy_addr) {
-                        Ok(future) => match future.await {
-                            Ok(mut stream) => match connect(&mut stream, addr_kind, auth).await {
-                                Ok(_) => Ok(stream),
-                                Err(error) => {
-                                    return Err(TorTransportError::SOCKS5Error(error));
-                                }
-                            },
-                            Err(error) => {
-                                return Err(TorTransportError::Transport(error));
-                            }
-                        },
-                        Err(TransportError::MultiaddrNotSupported(dest_addr)) => {
-                            return Err(TorTransportError::MultiaddrNotSupported(dest_addr));
-                        }
-                        Err(TransportError::Other(error)) => {
-                            return Err(TorTransportError::Transport(error));
-                        }
-                    }
-                } else {
-                    match inner.dial(dest_addr) {
-                        Ok(future) => future.await.map_err(|e| TorTransportError::Transport(e)),
-                        Err(error) => match error {
-                            TransportError::MultiaddrNotSupported(dest_addr) => {
-                                Err(TorTransportError::MultiaddrNotSupported(dest_addr))
-                            }
-                            TransportError::Other(error) => {
-                                Err(TorTransportError::Transport(error))
-                            }
-                        },
-                    }
+            if let Some((host, port)) = to_onion3_domain(dest_addr.clone()) {
+                match TorStream::connect_with_address(proxy_addr, (host.as_str(), port)) {
+                    Ok(connection) => Ok(TcpStream(tokio::net::TcpStream::from_std(
+                        connection.into_inner(),
+                    )?)),
+                    Err(error) => Err(TorTransportError::Transport(error)),
                 }
             } else {
-                match inner.dial(dest_addr) {
+                let dial = inner.lock().dial(dest_addr);
+                match dial {
                     Ok(future) => future.await.map_err(|e| TorTransportError::Transport(e)),
                     Err(error) => match error {
                         TransportError::MultiaddrNotSupported(dest_addr) => {
@@ -153,9 +122,10 @@ where
         &mut self,
         addr: Multiaddr,
     ) -> Result<Self::Dial, TransportError<Self::Error>> {
-        let mut inner = self.inner.clone();
+        let inner = self.inner.clone();
         Ok(async move {
-            match inner.dial_as_listener(addr) {
+            let dial = inner.lock().dial_as_listener(addr);
+            match dial {
                 Ok(future) => future.await.map_err(TorTransportError::Transport),
                 Err(error) => match error {
                     TransportError::MultiaddrNotSupported(addr) => {
@@ -169,14 +139,15 @@ where
     }
 
     fn address_translation(&self, listen: &Multiaddr, observed: &Multiaddr) -> Option<Multiaddr> {
-        self.inner.address_translation(listen, observed)
+        self.inner.lock().address_translation(listen, observed)
     }
 
     fn poll(
         mut self: std::pin::Pin<&mut Self>,
         cx: &mut std::task::Context<'_>,
     ) -> std::task::Poll<TransportEvent<Self::ListenerUpgrade, Self::Error>> {
-        Transport::poll(Pin::new(&mut self.inner), cx).map(|event| {
+        let mut inner = self.inner.lock();
+        Transport::poll(Pin::new(inner.deref_mut()), cx).map(|event| {
             event
                 .map_upgrade(|upgr| upgr.map_err::<_, fn(_) -> _>(TorTransportError::Transport))
                 .map_err(TorTransportError::Transport)
@@ -188,6 +159,7 @@ where
 mod tests {
     use super::*;
     use futures;
+    use libp2p_tcp::{GenTcpConfig, TokioTcpTransport};
     use socks5_proto::{Address, Reply};
     use socks5_server::{auth::NoAuth, Connection, Server};
     use std::sync::Arc;
@@ -216,9 +188,16 @@ mod tests {
             }
         });
 
-        let mut transport = TorTransport::new("/ip4/127.0.0.1/tcp/5000".parse().unwrap(), None);
+        let mut transport = TorTransportWrapper::new(
+            TokioTcpTransport::new(GenTcpConfig::default().nodelay(true)),
+            "127.0.0.1:5000".parse().unwrap(),
+        )?;
         let mut stream = transport
-            .dial("/dns/www.google.com/tcp/80".parse().unwrap())?
+            .dial(
+                "/onion3/vww6ybal4bd7szmgncyruucpgfkqahzddi37ktceo3ah7ngmcopnpyyd:1234"
+                    .parse()
+                    .unwrap(),
+            )?
             .await?;
 
         futures::AsyncWriteExt::write(&mut stream, "Ping!".as_bytes()).await?;
