9584fd5c5e99a7a155d7187c0da0aa8d69465375
Jack Lund <jackl@geekheads.net>
2022-08-14 12:40:13 -0500

    Get rid of content addressing

--- src/lib.rs
+++ src/lib.rs
@@ -129,20 +129,10 @@ impl Engine {
         transport: Boxed<(PeerId, StreamMuxerBox)>,
         peer_id: PeerId,
     ) -> Result<Self, std::io::Error> {
-        // To content-address message, we can take the hash of message and use it as an ID.
-        // TODO: This means we can't publish same message twice, probably not what we want, change
-        // this!!!
-        let message_id_fn = |message: &GossipsubMessage| {
-            let mut s = DefaultHasher::new();
-            message.data.hash(&mut s);
-            MessageId::from(s.finish().to_string())
-        };
-
         // Set a custom gossipsub
         let gossipsub_config = GossipsubConfigBuilder::default()
             .heartbeat_interval(Duration::from_secs(10)) // This is set to aid debugging by not cluttering the log space
             .validation_mode(ValidationMode::Strict) // This sets the kind of message validation. The default is Strict (enforce message signing)
-            .message_id_fn(message_id_fn) // content-address messages. No two messages of the same content will be propagated.
             .build()
             .expect("Valid config");
 
